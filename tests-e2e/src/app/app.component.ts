e0851a095fa1ca23aaac9ad9b8d793f670637c51
satanTime <m@sudo.eu>
2025-01-12 17:34:26 +0100

    fix(a19): support of Angular 19 #10503 #10632

--- tests-e2e/src/app/app.component.ts
+++ tests-e2e/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  standalone: false,
   template: 'ng-mocks:{{ title }}',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
--- tests-e2e/src/app/app.directive.ts
+++ tests-e2e/src/app/app.directive.ts
@@ -2,6 +2,7 @@ import { Directive } from '@angular/core';
 
 @Directive({
   selector: 'app-root',
+  standalone: false,
 })
 export class AppDirective {
   public appDirective() {}
